2981c330413b65f47d3c6d4090f54068f4e6af28
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-14 01:44:50 +0300

    main pages

---
+++ pages/events/[id].js
@@ -0,0 +1,9 @@
+function EventsDetailPage() {
+  return (
+    <div>
+      <h1>Event detail page</h1>
+    </div>
+  );
+}
+
+export default EventsDetailPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+function EventsPage() {
+  return (
+    <div>
+      <h1>Events page</h1>
+    </div>
+  );
+}
+
+export default EventsPage;
