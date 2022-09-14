6d826223873bb0bbf2334ebf681fc24e42791e1f
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-14 23:30:09 +0300

    all events page reuse components

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/eventList/event-list";
+
 function EventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>Events page</h1>
+      <EventList items={events} />
     </div>
   );
 }
