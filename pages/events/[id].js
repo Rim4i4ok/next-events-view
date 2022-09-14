5af1c4c2111fede7958b9b3634f20449aa01be87
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-14 22:56:07 +0300

    dynamic event page

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,8 +1,34 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import { getEventById } from "../../dummy-data";
+
 function EventsDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>Event not found</p>;
+  }
+
   return (
-    <div>
-      <h1>Event detail page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
