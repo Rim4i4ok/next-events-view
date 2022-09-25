3c707282d71f46245c455ed3ba2e874504cc0bb7
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-26 00:01:51 +0300

    all events static rendering

--- pages/events/index.js
+++ pages/events/index.js
@@ -3,11 +3,11 @@ import { Fragment } from "react";
 
 import EventList from "../../components/events/eventList/event-list";
 import EventsSearch from "../../components/events/eventsSearch/events-search";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 
-function EventsPage() {
+function EventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -21,6 +21,17 @@ function EventsPage() {
       <EventList items={events} />
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default EventsPage;
