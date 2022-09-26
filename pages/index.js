b6d7e4dd3e405bc2050b133c0718e3e640a128a9
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-27 00:25:00 +0300

    Head section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
+
 import EventList from "../components/events/eventList/event-list";
 import { getFeaturedEvents } from "../helpers/api-util";
 
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>Nextjs events</title>
+        <meta name="description" content="Hello world with events" />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
