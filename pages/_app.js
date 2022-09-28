202db6866d3743214e1d23777c92068e7ff92f34
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-28 21:58:30 +0300

    _app js head

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next events</title>
+        <meta name="description" content="Nestjs events" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
