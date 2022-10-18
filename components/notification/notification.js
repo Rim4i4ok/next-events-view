970ae7a6a71c69fdd55f9787d26327cba31e1306
Rim4i4ok <rim4i4ok@gmail.com>
2022-10-18 23:56:10 +0300

    removed not used context

--- components/notification/notification.js
+++ components/notification/notification.js
@@ -1,11 +1,6 @@
-import { useContext } from "react";
-
 import classes from "./notification.module.css";
-import NotificationContext from "../../store/notification-context";
 
 function Notification(props) {
-  const notificationCtx = useContext(NotificationContext);
-
   const { title, message, status } = props;
 
   let statusClasses = "";
@@ -25,7 +20,7 @@ function Notification(props) {
   const activeClasses = `${classes.notification} ${statusClasses}`;
 
   return (
-    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
+    <div className={activeClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
